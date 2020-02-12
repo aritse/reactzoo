666a077bb728096b0701816f23f77413fd52f7a0
Ari Tserendavaa <tserenda@hotmail.com>
2020-02-12 11:03:42 -0800

    created zoo app

---
+++ src/components/ZooList/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ZooItem from "../ZooItem";
+import "./style.css";
+
+export default function ZooList(props) {
+  return (
+    <div className="ZooList">
+      <div className="headers">
+        <h3>Headers go here</h3>
+      </div>
+      {props.animals.map(a => (
+        <ZooItem key={a.id} animal={a} />
+      ))}
+    </div>
+  );
+}
