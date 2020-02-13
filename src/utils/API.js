9d243e7ed1a3a54c6511f13109da70fee47a9fbb
Ari Tserendavaa <tserenda@hotmail.com>
2020-02-12 19:28:41 -0800

    updated url

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const URL = "http://localhost:8080";
-const URL = "https://reactzoo-api.herokuapp.com";
+const URL = "https://zoo-react-ari-api.herokuapp.com";
 
 const API = {
   getAllAnimals: () => axios.get(`${URL}/api/animals`),
